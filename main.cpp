f1b4fcbaaac5d931359e7e280b826c279a628717
Leon_Chao <liangzhao@buaa.edu.cn>
2021-07-23 11:51:29 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,267 @@
+//#include <glad/glad.h>
+#include <GL/glut.h>
+//#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include "glm/glm.hpp"
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+///*
+// *  init window
+// */
+//void init()
+//{
+//    glClearColor(0.0, 0.0, 0.0, 0.0);
+//    glMatrixMode(GL_PROJECTION);
+//    glOrtho(-5, 5, -5, 5, 5, 15);
+//    glMatrixMode(GL_MODELVIEW);
+//    gluLookAt(0, 0, 10, 0, 0, 0, 0, 1, 0);
+//}
+//
+//void display()
+//{
+//    glClear(GL_COLOR_BUFFER_BIT);
+//    glColor3f(1.0, 0, 0);
+//    glutWireTeapot(3);
+//    glFlush();
+//}
+//
+//int main(int argc, char *argv[])
+//{
+//    glutInit(&argc, argv);
+//    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+//    glutInitWindowPosition(0, 0);
+//    glutInitWindowSize(300, 300);
+//    glutCreateWindow("OpenGL 3D View");
+//    init();
+//    glutDisplayFunc(display);
+//    glutMainLoop();
+//
+//    return 0;
+//}
+#define PI 3.14159265359
+
+
+
+static GLuint texName[4];//存放纹理
+static double Ox = 0;
+static double Oy = 0;
+static double direction = 0;//站立位置及前进方向；
+static double step = 0.2;
+GLUquadricObj*g_text;//曲面，制作一个包围房间的大球体做背景
+
+
+
+void loadTexture(const char* filename,GLuint &texture)
+{
+    static GLint   ImageWidth;
+    static GLint   ImageHeight;
+    static GLint   PixelLength;
+    static GLubyte* PixelData;
+
+    // 打开文件
+    FILE* pFile = fopen(filename, "rb");
+    if (pFile == nullptr) {
+        printf("[Notice] File %s not found.", filename);
+        exit(0);
+    }
+
+    // 读取24位bmp图象的大小信息
+    fseek(pFile, 0x0012, SEEK_SET);
+    fread(&ImageWidth, sizeof(ImageWidth), 1, pFile);
+    fread(&ImageHeight, sizeof(ImageHeight), 1, pFile);
+
+    // 计算像素数据长度
+    PixelLength = ImageWidth * 3;
+    while (PixelLength % 4 != 0)//bmp图像保证数据区域的长度是4的倍数
+        ++PixelLength;
+    PixelLength *= ImageHeight;
+
+    // 读取像素数据
+    PixelData = (GLubyte*)malloc(PixelLength);
+    if (PixelData == nullptr)
+        exit(0);
+    fseek(pFile, 54, SEEK_SET);
+    fread(PixelData, PixelLength, 1, pFile);
+
+    // 关闭文件
+    fclose(pFile);
+
+    //加载纹理
+    glGenTextures(1, &texture);//用来生成纹理的数量  存储纹理索引的第一个元素指针
+    glBindTexture(GL_TEXTURE_2D, texture);//绑定（指定）纹理对象
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);   //纹理参数：坐标超过0.0~1.0f时的处理方式--重复
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);  //纹理参数：贴图插值方式--最近邻 / 线性插值 GL_LINEAR
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, ImageWidth, ImageHeight, 0, GL_BGR_EXT, GL_UNSIGNED_BYTE, PixelData);
+
+    //生成一个2D纹理（Texture）。bmp图片的像素顺序是BGR所以用GL_BGR_EXT来反向加载数据
+
+}
+
+void init()
+{
+    glClearColor(0.0, 0.0, 0.0, 0.0);//清除颜色缓冲区，设置颜色
+    glShadeModel(GL_FLAT);//设置着色模式 GL_FLAT 恒定着色，GL_SMOOTH光滑着色
+    glEnable(GL_DEPTH_TEST);
+    g_text = gluNewQuadric();//这句代码需放置在绘图及显示之前
+    loadTexture("../resources/floor.bmp",texName[0]);//图片与程序源代码放置在同一目录下即可
+    loadTexture("../resources/wall.bmp", texName[1]);
+    loadTexture("../resources/ceiling.bmp",texName[2]);
+    loadTexture("../resources/back.bmp", texName[3]);
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);//所读取的图像数据的行对齐方式
+}
+
+// 定义纹理模型并刷新双缓存
+void display()
+{
+    // 清除屏幕
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);//清除颜色缓冲以及深度缓冲
+    // 启用二维纹理
+    glEnable(GL_TEXTURE_2D);
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);//纹理和材质混合方式
+    glPushMatrix(); // 保存当前模型视图矩阵
+    glRotatef((GLfloat)direction, 0.0, 0.0, 1.0);   // 用变换后(旋转或移动)模型坐标系建立模型
+    glTranslated(Ox, Oy, 0.0);
+
+    // 创建背景球体
+    glBindTexture(GL_TEXTURE_2D, texName[3]);
+    gluSphere(g_text, 30, 15, 15);
+    gluQuadricTexture(g_text, GLU_TRUE);             //建立纹理坐标
+    gluQuadricDrawStyle(g_text, GLU_FILL);           //用面填充
+
+    // 地面部分
+    glBindTexture(GL_TEXTURE_2D, texName[0]);//floor
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0, 0.0);glVertex3f(-10.0, -10.0, 0.0);
+    glTexCoord2f(0.0, 1.0);glVertex3f(-10.0, 10.0, 0.0);
+    glTexCoord2f(1.0, 1.0);glVertex3f(10.0, 10.0, 0.0);
+    glTexCoord2f(1.0, 0.0);glVertex3f(10.0, -10.0, 0.0);
+    glEnd();
+
+    // 墙体部分
+    glBindTexture(GL_TEXTURE_2D, texName[1]);//wall
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0, 0.0);glVertex3f(-10.0, -10.0, 0.0);
+    glTexCoord2f(0.0, 10.0);glVertex3f(-10.0, -10.0, 10.0);
+    glTexCoord2f(10.0, 10.0);glVertex3f(-10.0, 10.0, 10.0);
+    glTexCoord2f(10.0, 0.0);glVertex3f(-10.0, 10.0, 0);
+
+    glTexCoord2f(0.0, 0.0);glVertex3f(10.0, -10.0, 0.0);
+    glTexCoord2f(0.0, 10.0);glVertex3f(10.0, -10.0, 10.0);
+    glTexCoord2f(10.0, 10.0);glVertex3f(10.0, 10.0, 10.0);
+    glTexCoord2f(10.0, 0.0);glVertex3f(10.0, 10.0, 0);
+
+    glTexCoord2f(0.0, 0.0);glVertex3f(-10.0, 10.0, 0.0);
+    glTexCoord2f(0.0, 10.0);glVertex3f(-10.0, 10.0, 10.0);
+    glTexCoord2f(10.0, 10.0);glVertex3f(10.0, 10.0, 10.0);
+    glTexCoord2f(10.0, 0.0);glVertex3f(10.0, 10.0, 0.0);
+
+    glTexCoord2f(0.0, 0.0);glVertex3f(-10.0, -10.0, 0.0);
+    glTexCoord2f(0.0, 10.0);glVertex3f(-10.0, -10.0, 10.0);
+    glTexCoord2f(10.0, 10.0);glVertex3f(10.0, -10.0, 10.0);
+    glTexCoord2f(10.0, 0.0);glVertex3f(10.0, -10.0, 0);
+    glEnd();
+
+    // 封顶部分
+    glBindTexture(GL_TEXTURE_2D, texName[2]);//ceiling
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0, 0.0);glVertex3f(-10.0, -10.0, 10.0);
+    glTexCoord2f(0.0, 1.0);glVertex3f(-10.0, 10.0, 10.0);
+    glTexCoord2f(1.0, 1.0);glVertex3f(10.0, 10.0, 10.0);
+    glTexCoord2f(1.0, 0.0);glVertex3f(10.0, -10.0, 10.0);
+    glEnd();
+
+    glPopMatrix();  // 恢复保存的视角
+    glFlush();//用于强制刷新缓存
+    glutSwapBuffers();//交换双缓存
+    glutPostRedisplay();//标志下次刷新，没有的话，程序打开后不会刷新界面
+}
+
+// 在窗口改变时调整投影矩阵(视角)，模型矩阵(glulookat)
+// w, h 是窗口的宽高
+void reshape(int w, int h)
+{
+// viewport settings
+    glViewport(0, 0, (GLsizei)w, (GLsizei)h);//(设置显示区域的大小)在默认情况下，视口被设置为占据打开窗口的整个像素矩形
+
+// projection settings
+    glMatrixMode(GL_PROJECTION);//选择投影矩阵
+    glLoadIdentity();//重置当前指定的矩阵为单位矩阵
+    gluPerspective(90.0, (GLfloat)w / (GLfloat)h, 0.1, 300.0);//(设置虚拟投影面的宽高比)
+    // 一般和视口尺寸一致，否则会引起拉伸
+
+// model / viewpoint settings
+    glMatrixMode(GL_MODELVIEW);//模型视景矩阵||GL_TEXTURE,对纹理矩阵堆栈应用随后的矩阵操作.
+    glLoadIdentity();
+    //通过设置视点来设置模型坐标系
+    gluLookAt(0, 0, 3, 0,  10, 3, 0.0, 0.0, 1.0);
+}
+
+// 定义键盘事件时的动作
+void keyboard(unsigned char key, int x, int y)
+{
+    switch (key) {
+        case 'w':
+        case 'W':
+            Ox -= step*sin(direction / 180 * PI);
+            Oy -= step*cos(direction / 180 * PI);
+            glutPostRedisplay();
+            break;
+        case 's':
+        case 'S':
+            Ox += step*sin(direction/180*PI);
+            Oy += step*cos(direction/180*PI);
+            glutPostRedisplay();
+            break;
+        case 'a':
+        case 'A':
+            direction = direction - 2;
+            glutPostRedisplay();
+            break;
+        case 'd':
+        case 'D':
+            direction = direction + 2;
+            glutPostRedisplay();
+            break;
+        case 27:
+            exit(0);
+        default:
+            break;
+    }
+}
+
+void print_version(){
+    const GLubyte* name = glGetString(GL_VENDOR); //返回负责当前OpenGL实现厂商的名字
+    const GLubyte* biaoshifu = glGetString(GL_RENDERER); //返回一个渲染器标识符，通常是个硬件平台
+    const GLubyte* OpenGLVersion =glGetString(GL_VERSION); //返回当前OpenGL实现的版本号
+    const GLubyte* gluVersion= gluGetString(GLU_VERSION); //返回当前GLU工具库版本
+    GLint max;
+    glGetIntegerv(GL_MAX_TEXTURE_SIZE, &max);
+    printf("OpenGL实现厂商的名字：%s\n", name);
+    printf("渲染器标识符：%s\n", biaoshifu);
+    printf("OOpenGL实现的版本号：%s\n",OpenGLVersion );
+    printf("OGLU工具库版本：%s\n", gluVersion);
+    printf("最大支持图片像素个数：%d\n", max);
+}
+
+int main(int argc, char* argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(800, 800);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow(argv[0]);
+
+    init();
+    print_version();
+    glutDisplayFunc(&display);
+//    glutIdleFunc(&display);
+    glutReshapeFunc(&reshape);
+    glutKeyboardFunc(&keyboard);
+    glutMainLoop();
+
+    return 0;
+}
---
+++ rawcode.cpp
@@ -0,0 +1,333 @@
+//基于OpenGL投算法实现
+//#include <Windows.h>
+#include <GL/glut.h>
+//#include <GL/glaux.h>
+#include <cv.h>
+#include <cxcore.h>
+#include <opencv2/imgcodecs/imgcodecs_c.h>
+//#include <highgui\highgui_c.h>
+//#include <core\types_c.h>
+#include <stdio.h>
+//#include "API912.h"
+//#include "Type912.h"
+//#include "calcsurface.h"
+//#include "28.h"
+#pragma comment(lib, "opencv_core231d.lib")
+#pragma comment(lib, "opencv_imgproc231d.lib")
+#pragma comment(lib, "opencv_highgui231d.lib")
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "glu32.lib")
+#pragma comment(lib, "glaux.lib")
+#define MAXSIZE 259200
+#define MAX_FILE_PATH 200
+void myinit(void);
+void makeImage(void);
+void DrawMyObjects(void);
+//void CALLBACK myReshape(GLsizei w, GLsizei h);
+//void CALLBACK display(void);
+//void CALLBACK rotate(AUX_EVENTREC *event);
+void myReshape(GLsizei w, GLsizei h);
+void display(void);
+void rotate(AUX_EVENTREC *event);
+GLubyte
+    imagedata0[MAXSIZE],
+    imagedata1[MAXSIZE], imagedata2[MAXSIZE], imagedata3[MAXSIZE];
+GLsizei width = 360, height = 240;
+GLint i, ii, p, n, Model;
+TVSInfo stTVSInfo;  // 标定参数
+int Option = 100, dx = 700, dy = 700, dz = 700,
+    dx1 = 700, dy1 = 600, dz1 = 700,
+    dx2 = 700, dy2 = 600, dz2 = 700;
+//dx = 700;
+//dy = 700;
+//dz = 700;
+//dx1 = 700;
+//dy1 = 600;
+//dz1 = 700;
+//dx2 = 700;
+//dy2 = 600;
+//dz2 = 700;
+//Option = 100;
+IplImage *image[4];
+GLuint texture[4];
+
+// 定义纹理数据
+void makeImage(void)
+{
+    image[0] = cvLoadImage("F:\\A56test\\22708\\tail.bmp", -1);
+    image[1] = cvLoadImage("F:\\A56test\\22708\\front.bmp", -1);
+    image[2] = cvLoadImage("F:\\A56test\\22708\\left.bmp", -1);
+    image[3] = cvLoadImage("F:\\A56test\\22708\\right.bmp", -1);
+    for (p = 0; p < image[0]->height; p++)
+    {
+        GLubyte *ptr = (GLubyte *)(image[0]->imageData + p * image[0]->widthStep);
+        for (n = 0; n < image[0]->width; n++)
+        {
+            // write R G B value into imagedata[], 1D array.
+            imagedata0[p * image[0]->widthStep + n * 3 + 0] = ptr[3 * n + 2];
+            imagedata0[p * image[0]->widthStep + n * 3 + 1] = ptr[3 * n + 1];
+            imagedata0[p * image[0]->widthStep + n * 3 + 2] = ptr[3 * n + 0];
+        }
+    }
+
+    glGenTextures(1, &texture[0]);            // 为第i个位投创建纹理
+    glBindTexture(GL_TEXTURE_2D, texture[0]);     // 将生成投纹理投名称绑定投指定投纹理上
+    glTexImage2D(GL_TEXTURE_2D, 0, 3, 360, 240, 0, GL_RGB, GL_UNSIGNED_BYTE,
+                 imagedata0);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, 0x812F);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, 0x812F);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+    glGenTextures(1, &texture[1]); // 为第i个位投创建纹理
+}
+
+// Initialize
+void myinit()
+{
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    makeImage();    // 载入纹理
+    glEnable(GL_TEXTURE_2D);
+    glShadeModel(GL_SMOOTH);
+    Model = GL3DS_initialize_28();  // 生成模型的显示列表，保存显示列表号
+}
+
+//void CALLBACK myReshape(GLsizei w, GLsizei h)
+void myReshape(GLsizei w, GLsizei h)
+{
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(70, 1, 0.1, 80);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    //OMINI
+    glScalef(3, 3, 1);
+    gluLookAt(0, -40, 45, 0, 0, 0, 0, 1, 0);
+    //front1
+    // glScalef(2.5,2.5,1);
+    // glTranslatef(0,0,27);
+    // gluLookAt(0,-29,15,0,10,0,0,1,0);
+    //front2
+    // glScalef(2.5,2.5,1);
+    // glTranslatef(0,0,27);
+    // gluLookAt(0,-37,15,0,10,0,0,1,0);
+    //left
+    // glScalef(2.5,2.5,1);
+    // glTranslatef(0,0,23);
+    // glRotatef(-6,0,1,0);
+    // gluLookAt(0,-35,15,0,12,0,0,1,0);
+    //right
+    // glScalef(3,3,1);
+    // glTranslatef(0,0,23);
+    // glRotatef(6,0,1,0);
+    // gluLookAt(0,-40,15,0,12,0,0,1,0);
+    //back
+    // glScalef(3,3,1);
+    // glTranslatef(0,-10,9);
+    // glRotatef(180,0,0,1);
+    // gluLookAt(0,40,15,0,10,0,0,1,0);
+}
+
+// render textures
+//void CALLBACK display(void)
+void display(void)
+{
+    glColor3f(0.5, 0.5, 0.5);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    DrawMyObjects();    // 绘制模型并纹理映射
+    glFlush();
+}
+
+void DrawMyObjects(void)
+{
+    GLfloat m[222], m1[222], m2[222], m3[222], m4[222], m5[222], m6[222];
+    GLfloat mm[222], mm1[222], mm2[222], mm3[222], mm4[222];
+    GLint i, j, k, n, p;
+    GLdouble pWorld[4], pCam[4], pCame[4], pImg[2];
+    GLfloat smA[18], smA1[18], smA2[18], smA3[18], smA4[18];
+    GLfloat smB[16], smB1[16], smB2[16], smB3[16], smB4[16];
+    GLfloat smC[16], smC1[16], smC2[16], smC3[16], smC4[16];
+    GLfloat smD[16], smD1[16], smD2[16], smD3[16], smD4[16];
+    GLfloat colorAC[24] = {1.0, 1.0, 0.9, 1.0, 1.0, 1.0, 1.0, 0.9,
+                           1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0,
+                           1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0};
+    GLfloat colorBD[24] = {1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0,
+                           1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0,
+                           1.0, 1.0, 1.0, 0.9, 1.0, 1.0, 1.0, 0.9};
+    p = 0;
+    glRotatef(-10, 1.0f, 0.0f, 0.0f);
+    ThreeSurface(mm, mm1, mm2, mm3, mm4, m, m1, m2, m3); //获取三维曲面顶投数据
+
+    // 计算映射坐标
+    for (i = 36; i <= 75; i += 3)   // 13个点
+    {
+        pWorld[0] = -m[i + 1] * dx1;
+        pWorld[1] = m[i] * dy1;
+        pWorld[2] = m[i + 2] * dz1;
+        pWorld[3] = 1;
+        matrixMul(&stTVSInfo.staCameras[1].stCameraParamEXT.daExtParam, pWorld, pCam, 4,
+                  4, 1);
+        for (j = 0; j <= 2; j++)
+        {
+            pCame[j] = pCam[j];
+        }
+        Cam2ImgScara(pImg, pCame, &stTVSInfo.staCameras[0]);
+        pImg[1] = pImg[1] * 360 / 320; // 原尺寸为 360x320， 将dim2统一到360 下面是归一化
+        s[2 * p + 24 + 0] = pImg[1] > 0 ? (pImg[1] <= 360 ? (pImg[1] / 360) : 1) : 0;
+        s[2 * p + 24 + 1] = pImg[0] > 0 ? (pImg[0] <= 240 ? (pImg[0] / 240) : 1) : 0;
+        p++;
+    }
+    p = 0;
+    for (i = 72; i <= 147; i += 3)  // 25 points
+    {
+        pWorld[0] = -m1[i + 1] * dx;
+        pWorld[1] = m1[i] * dy;
+        pWorld[2] = m1[i + 2] * dz;
+        pWorld[3] = 1;
+        matrixMul(&stTVSInfo.staCameras[2].stCameraParamEXT.daExtParam, pWorld, pCam, 4,
+                  4, 1);
+        for (j = 0; j <= 2; j++)
+        {
+            pCame[j] = pCam[j];
+        }
+        Cam2ImgScara(pImg, pCame, &stTVSInfo.staCameras[0]);
+        pImg[1] = pImg[1] * 360 / 320;
+        s1left[2 * p + 48 + 0] = pImg[1] > 0 ? (pImg[1] <= 360 ? (pImg[1] / 360) : 1) : 0;
+        s1left[2 * p + 48 + 1] = pImg[0] > 0 ? (pImg[0] <= 240 ? (pImg[0] / 240) : 1) : 0;
+        p++;
+    }
+    p = 0;
+    for (i = 144; i <= 183; i += 3)     // 13 points
+    {
+        pWorld[0] = -m1[i + 1] * dx2;
+        pWorld[1] = m1[i] * dy2;
+        pWorld[2] = m1[i + 2] * dz2;
+        pWorld[3] = 1;
+        matrixMul(&stTVSInfo.staCameras[0].stCameraParamEXT.daExtParam, pWorld, pCam, 4,
+                  4, 1);
+        for (j = 0; j <= 2; j++)
+        {
+            pCame[j] = pCam[j];
+        }
+        Cam2ImgScara(pImg, pCame, &stTVSInfo.staCameras[0]);
+        pImg[1] = pImg[1] * 360 / 320;
+        s1rear[2 * p + 96 + 0] = pImg[1] > 0 ? (pImg[1] <= 360 ? (pImg[1] / 360) : 1) : 0;
+        s1rear[2 * p + 96 + 1] = pImg[0] > 0 ? (pImg[0] <= 240 ? (pImg[0] / 240) : 1) : 0;
+        p++;
+    }
+    p = 0;
+    for (i = 180; i <= 255; i += 3)     // 25 points
+    {
+        ii = i;
+        if (ii > 219)
+        {
+            ii = i - 222 + 6;
+        }
+        pWorld[0] = -m1[ii + 1] * dx;
+        pWorld[1] = m1[ii] * dy;
+        pWorld[2] = m1[ii + 2] * dz;
+        pWorld[3] = 1;
+        matrixMul(&stTVSInfo.staCameras[3].stCameraParamEXT.daExtParam, pWorld, pCam, 4,
+                  4, 1);
+        for (j = 0; j <= 2; j++)
+        {
+            pCame[j] = pCam[j];
+        }
+        Cam2ImgScara(pImg, pCame, &stTVSInfo.staCameras[0]);
+        pImg[1] = pImg[1] * 360 / 320;
+        s1right[2 * p + 120 + 0] = pImg[1] > 0 ? (pImg[1] <= 360 ? (pImg[1] / 360) : 1) : 0;
+        s1right[2 * p + 120 + 1] = pImg[0] > 0 ? (pImg[0] <= 240 ? (pImg[0] / 240) : 1) : 0;
+        p++;
+    }
+    //接缝处
+    p = 0;
+    for (i = 72; i <= 87; i += 3)   // 5 points
+    {
+        pWorld[0] = -m[i + 1] * dx1;
+        pWorld[1] = m[i] * dy1;
+        pWorld[2] = m[i + 2] * dz1;
+        pWorld[3] = 1;
+        matrixMul(&stTVSInfo.staCameras[1].stCameraParamEXT.daExtParam, pWorld, pCam, 4,
+                  4, 1);
+        for (j = 0; j <= 2; j++)
+        {
+            pCame[j] = pCam[j];
+        }
+        Cam2ImgScara(pImg, pCame, &stTVSInfo.staCameras[0]);
+        pImg[1] = pImg[1] * 360 / 320;
+        sA[2 * p + 0] = pImg[1] > 0 ? (pImg[1] <= 360 ? (pImg[1] / 360) : 1) : 0;
+        sA[2 * p + 1] = pImg[0] > 0 ? (pImg[0] <= 240 ? (pImg[0] / 240) : 1) : 0;
+        p++;
+    }
+    p = 0;
+    for (i = 72; i <= 87; i += 3)       //  5 points
+    {
+        pWorld[0] = -m1[i + 1] * dx1;
+        pWorld[1] = m1[i] * dy1;
+        pWorld[2] = m1[i + 2] * dz1;
+        pWorld[3] = 1;
+        matrixMul(&stTVSInfo.staCameras[1].stCameraParamEXT.daExtParam, pWorld, pCam, 4,
+                  4, 1);
+        for (j = 0; j <= 2; j++)
+        {
+            pCame[j] = pCam[j];
+        }
+        Cam2ImgScara(pImg, pCame, &stTVSInfo.staCameras[0]);
+        pImg[1] = pImg[1] * 360 / 320;
+        sA1[2 * p + 0] = pImg[1] > 0 ? (pImg[1] <= 360 ? (pImg[1] / 360) : 1) : 0;
+        sA1[2 * p + 1] = pImg[0] > 0 ? (pImg[0] <= 240 ? (pImg[0] / 240) : 1) : 0;
+        p++;
+    }
+
+    //纹理映入
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+    glBindTexture(GL_TEXTURE_2D, texture[1]); //1front //2left //0tail //3right
+    glVertexPointer(3, GL_FLOAT, 0, m);
+    glTexCoordPointer(2, GL_FLOAT, 0, s);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 74);
+
+    //接缝处
+    glEnableClientState(GL_COLOR_ARRAY);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    //glColor4f(1.0f,1.0f,1.0f,0.7f);
+    glColorPointer(4, GL_FLOAT, 0, colorAC);
+    glBindTexture(GL_TEXTURE_2D, texture[1]);
+    glVertexPointer(3, GL_FLOAT, 0, m + 72);
+    glTexCoordPointer(2, GL_FLOAT, 0, sA);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 6);
+
+    glDisableClientState(GL_COLOR_ARRAY);
+    glDisableClientState(GL_VERTEX_ARRAY);
+    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+    glDisable(GL_TEXTURE_2D);
+    glCallList(Model);  // 调用显示列表
+    glDisable(GL_BLEND);
+}
+
+// catch keyboard/mouse event
+//void CALLBACK rotate(AUX_EVENTREC *event)
+void rotate(AUX_EVENTREC *event)
+{
+    GLint x, y;
+    myinit();
+    x = event->data[AUX_MOUSEX];
+    y = event->data[AUX_MOUSEY];
+}
+
+int main(void)
+{
+    TvsLibSetDefaultParam(&stTVSInfo, Option);
+    auxInitDisplayMode(AUX_SINGLE | AUX_RGBA);
+    auxInitPosition(0, 0, 400, 400);
+    auxInitWindow("Geometric Primitive Types");
+    myinit();
+    auxReshapeFunc(myReshape);
+    auxMouseFunc(AUX_LEFTBUTTON, AUX_MOUSEDOWN, rotate);
+    auxMainLoop(display);
+}
